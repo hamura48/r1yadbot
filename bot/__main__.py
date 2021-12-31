d24805979e243d81eb8c6937f4126c6af82bb13e
hamura48 <90378399+hamura48@users.noreply.github.com>
2021-12-31 20:21:59 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -62,8 +62,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+MwgSi5vmQEA2N2Vk")
+    buttons.buildbutton("Contact me", "https://t.me/riyad48")
+    buttons.buildbutton("Official Group", "https://t.me/mirror_rtx")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
@@ -173,7 +173,7 @@ help_string_telegraph = f'''<br>
 '''
 
 help = telegraph.create_page(
-        title='Mirror-Leech-Bot Help',
+        title='Public Mirror Bot Help manual',
         content=help_string_telegraph,
     )["path"]
 
